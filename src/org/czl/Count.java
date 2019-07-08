97afc09529fbc1cb2fb5b9177d9acabb249456dd
wolf <chenzilin97@gmail.com>
2019-07-08 16:23:28 +0800

    addCount

---
+++ src/org/czl/Count.java
@@ -0,0 +1,15 @@
+package org.czl;
+
+public class Count {
+    int a;
+    int b;
+    public Count() {
+    }
+    public Count(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public void countTools(){
+        System.out.println(a + "——" + b);
+    }
+}
